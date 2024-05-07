7882acd0fb492f80350ac90101cd20d3d2006b0c
shanoukhavi <shanoukh5@gmail.com>
2024-05-07 22:34:13 +0530

    changes done in arcade file

--- Rockpapers/arcade.py
+++ Rockpapers/arcade.py
@@ -1,52 +1,39 @@
 import sys
+import argparse
 from rps8 import rps
 from guessmynumbermodified import guess
 
 
 def play_game(name="PlayerOne"):
-    wc = False
+    welcome_back = False
+
     while True:
-        if wc == True:
+        if welcome_back:
             print(f"\n{name}, welcome back to the Arcade menu.")
 
-        playerchoice = input(
+        player_choice = input(
             '\nPlease choose a game:\n1 = Rock Paper Scissors\n2 = Guess My Number\n\nOr press "x" to exit the Arcade\n\n'
         )
 
-        if playerchoice not in ["1", "2", "x"]:
-            print(f"\n{name}, please enter 1, 2, or x.")
-            return play_game(name)
-        wc = True
+        if player_choice not in ["1", "2", "x"]:
+            print(f"\n{name}, please enter 1, 2, or 'x'.")
+            continue
 
-        if playerchoice == "1":
-            rock_paper_scissors = rps(name)
-            rock_paper_scissors()
-        elif playerchoice == "2":
-            guess_my_number = guess(name)
-            guess_my_number()
-        else:
-            print("\nSee you next time!\n")
-            sys.exit(f"Bye {name}! 👋")
+        if player_choice == "x":
+            print(f"\nSee you next time, {name}!\n")
+            sys.exit("Bye! 👋")
+
+        game = rps(name) if player_choice == "1" else guess(name)
+        game()
+
+        welcome_back = True
 
 
 if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser(
-        description="Provides a personalized game experience."
-    )
-
-    parser.add_argument(
-        "-n",
-        "--name",
-        metavar="name",
-        required=True,
-        help="The name of the person playing the game.",
-    )
-
+    parser = argparse.ArgumentParser(description="Provides a personalized game experience.")
+    parser.add_argument("-n", "--name", metavar="name", required=True, help="The name of the person playing the game.")
     args = parser.parse_args()
 
     print(f"\n{args.name}, welcome to the Arcade! 🤖")
+    play_game(args.name)
 
-    unini = play_game(args.name)
-    unini()
